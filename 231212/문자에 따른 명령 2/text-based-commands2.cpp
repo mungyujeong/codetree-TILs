1952e8f3e76b221664798caf07773b7eea5065b8
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-12 20:41:15 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/문자에 따른 명령 2/text-based-commands2.cpp
+++ 231212/문자에 따른 명령 2/text-based-commands2.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 string cmd;
 // 동 남 서 북
-int dx[DIR_NUM] = {0, 1, 0, -1};
+int dx[DIR_NUM] = {0, -1, 0, 1};
 int dy[DIR_NUM] = {1, 0, -1, 0};
 
 int main() {
     cin >> cmd;
     int x = 0;
-    int y =0;
+    int y = 0;
     int d = {3};
     for (int i = 0 ; i < cmd.size(); i ++ ) {
         if (cmd[i] == 'L') {
