2a151034fccf024d8b5c8b6e20bcf8fc232fe4a0
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-13 20:11:15 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231213/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 231213/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -10,31 +10,29 @@ int dx[DIR_NUM] = {0, -1, 0, 1};
 int dy[DIR_NUM] = {1, 0, -1, 0};
 int arr[MAX_N][MAX_N];
 
-bool IsRange(int x, int y){
+bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
 int main() {
     cin >> n;
     x = y = int(n / 2);
-    count = 2;
-    arr[x][y] = 1;
-    dist = cycle = 1;
-    while(count <= n * n) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        if (IsRange(nx, ny) && arr[nx][ny] == 0 && dist != 0) {
-            x = nx; y = ny;
-            arr[nx][ny] = count++;
-            dist -= 1;
-        } else {
-            dir = (dir + 1) % 4;
-            if (dir == 0 || dir == 2) {
-                cycle++;
-            }
-            dist = cycle;
+    count = 1;
+    dist = 1;
+
+    while(InRange(x, y)) {
+        for (int i = 0; i < dist; i++) {
+            arr[x][y] = count++;
+            x += dx[dir]; y += dy[dir];
+
+            if(!InRange(x, y)) 
+                break;
         }
-    }    
+        dir = (dir + 1) % 4;
+        if (dir == 0 || dir == 2) 
+            dist++;
+    }
+    
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) 
