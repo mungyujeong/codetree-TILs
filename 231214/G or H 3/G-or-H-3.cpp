70bef6c34132cbccc0fec522bce71c4ee3d50b6d
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-14 15:58:43 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <climits>
+
+#define MAX_P 10000
+
+using namespace std;
+
+int n, k, num, max_x;
+char word;
+int position[MAX_P];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> num >> word;
+        if (word == 'G') {
+            position[num] = 1;
+        } else if (word == 'H'){
+            position[num] = 2;
+        } else {
+            position[num] = 0;
+        }
+        max_x = max(max_x, num);
+    }
+
+    int max_score = INT_MIN;
+    
+    for (int i = 1; i <= max_x - k; i++) {
+        int sum = 0;
+        for (int j = i; j < i + k + 1 ; j++) {
+            sum += position[j];
+        }
+        max_score = max(max_score, sum);
+    }
+
+    cout << max_score;
+
+    return 0;
+}
