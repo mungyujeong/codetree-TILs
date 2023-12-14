b4511ab131119dc82588fab31bdb2a03884ac1b7
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-14 21:50:33 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+
+#define MAX_NUM 100
+
+using namespace std;
+
+int n, h, t, min_sum = INT_MAX;
+int arr[MAX_NUM];
+
+int main() {
+    cin >> n >> h >> t;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    for (int i = 0; i < n - t; i++) {
+        int sum = 0;
+        for (int j = i; j < i + t; j++) {
+            sum += abs(t - arr[j]);
+        }
+        min_sum = min(min_sum, sum);
+    }
+    cout << min_sum;
+    return 0;
+}
