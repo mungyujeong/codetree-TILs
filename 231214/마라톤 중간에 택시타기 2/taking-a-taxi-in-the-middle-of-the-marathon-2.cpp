58ea885f9f7beaf254dd02ca635a4f47cf0b6e02
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-14 11:46:19 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+#define MAX_N 100
+
+int n, curr_x, curr_y;
+int max_dist = INT_MIN;
+pair<int, int> arr[MAX_N];
+
+int GetDist(int sx, int sy, int dx, int dy) {
+    return abs(sx - dx) + abs(sy - dy);
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        arr[i] = make_pair(x, y);
+        // cout << arr[i].first << arr[i].second << endl;
+    }
+
+    curr_x = arr[0].first; curr_y = arr[0].second;
+    for (int i = 1; i < n; i++) {
+        int dist = 0;
+        for (int j = 1; j < n; j++) {
+            if (i == j) {
+                continue;
+            } else {
+                dist += GetDist(curr_x, curr_y, arr[j].first, arr[j].second);
+                curr_x = arr[j].first;
+                curr_y = arr[j].second;
+            }
+        }
+        max_dist = max(max_dist, dist);
+    }
+    cout << max_dist;
+    return 0;
+}
