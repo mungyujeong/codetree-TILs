c04152c466ae4a8a7816810c26444751555779ac
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-14 14:23:40 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+
+#define MAX_N 20
+
+using namespace std;
+
+int max_sum = INT_MIN;
+int n;
+int grid[MAX_N][MAX_N];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 2; j++) {
+            for (int k = i + 1; k < n; k++) {
+                for (int l = j + 1; l < n; l++) {
+                    max_sum = max(max_sum, grid[i][j] + grid[i][j + 1] + grid[i][j + 2] +
+                                            grid[k][l] + grid[k][l + 1] + grid[k][l + 2]);
+                }
+            }
+        }
+    }
+    cout << max_sum;
+    return 0;
+}
