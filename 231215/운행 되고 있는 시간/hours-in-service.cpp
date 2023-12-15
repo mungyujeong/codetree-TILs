abe50200fa68cb3261465ed310ac9a487813a9fe
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-15 21:08:44 +0900

    Title: 운행 되고 있는 시간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231215/운행 되고 있는 시간/hours-in-service.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n, answer;
+int a[MAX_N];
+int b[MAX_N];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i] >> b[i];
+    
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        bool visited[MAX_N] = {true, };
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            for (int t = a[j]; t < b[j]; t++) {
+                if (visited[t]) continue;
+                else {
+                    sum++;
+                    visited[t] = true;
+                }
+            }
+        }
+
+        answer = max(answer, sum);
+    }
+    cout << answer;
+    return 0;
+}
