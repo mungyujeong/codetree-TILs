62ac293184753ef0d30175a410b9a7561dc14f70
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-15 20:37:23 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231215/삼각형 만들기/create-triangle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n, answer;
+int x[MAX_N];
+int y[MAX_N];
+
+int IsValid(int i, int j, int k) {
+    if (x[i] == x[j] && y[i] == y[k]) return true;
+    if (x[i] == x[j] && y[j] == y[k]) return true;
+    if (x[i] == x[k] && y[i] == y[j]) return true;
+    if (x[i] == x[k] && y[k] == y[j]) return true;
+    if (x[j] == x[k] && y[j] == y[i]) return true;
+    if (x[j] == x[k] && y[k] == y[i]) return true;
+
+    return false;
+}
+
+int GetValue(int i, int j, int k) {
+    return 0.5 * abs((x[i] * y[j] + x[j] * y[k] + x[k] * y[i]) - (x[j] * y[i] + x[k] * y[j] + x[i] * y[k]));
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> x[i] >> y[i];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (IsValid(i, j, k)) {
+                    answer = max(answer, GetValue(i, j, k));
+                }
+            }
+        }
+    }
+    cout << answer * 2;
+    return 0;
+}
