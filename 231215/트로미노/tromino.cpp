e0ad53c4954a341c8696119b4cefebbeb97c5a5c
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-15 12:14:11 +0900

    Title: 트로미노, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 231215/트로미노/tromino.cpp
+++ 231215/트로미노/tromino.cpp
@@ -7,47 +7,57 @@ using namespace std;
 int n, m, answer, count, tmp;
 int grid[MAX_N][MAX_N];
 
+int shape[6][3][3] = {
+    {{1, 1, 1},
+     {0, 0, 0},
+     {0, 0, 0}}, 
+    {{1, 0, 0},
+     {1, 0, 0},
+     {1, 0, 0}},
+    {{1, 1, 0},
+     {1, 0, 0},
+     {0, 0, 0}},
+    {{1, 1, 0},
+     {0, 1, 0},
+     {0, 0, 0}},
+    {{1, 0, 0},
+     {1, 1, 0},
+     {0, 0, 0}},
+    {{0, 1, 0},
+     {1, 1, 0},
+     {0, 0, 0}},
+};
+
+int GetTropino(int x, int y) {
+    int max_sum = 0;
+    for (int i = 0; i < 6; i++) {
+        int count = 0;
+        bool isPossible = true;
+        for (int dx = 0; dx < 3; dx++) {
+            for (int dy = 0; dy < 3; dy++) {
+                if (shape[i][dx][dy] == 0) continue;
+                else if (x + dx >= n || y + dy >= m) isPossible = false;
+                else {
+                    count += grid[x + dx][y + dy];
+                }
+            }
+        }
+
+        if (isPossible) 
+            max_sum = max(max_sum, count);
+    }
+    return max_sum;
+}
+
 int main() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) 
         for (int j = 0; j < m; j++) 
             cin >> grid[i][j];
     
-    // 가로 막대
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= m - 3; j++) {
-            count = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
-            answer = max(answer, count);
-        }
-    }
-
-    // 세로 막대
-    for (int j = 0; j < m; j++) {
-        for (int i = 0; i <= n - 3; i++) {
-            count = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
-            answer = max(answer, count);
-        }
-    }
-
-    // 계단 블럭
-    for (int i = 0; i <= n - 2; i++) {
-        for (int j = 0; j <= m - 2; j++) {
-            count = 0;
-            for (int k = i; k < i + 2; k++) {
-                for (int l = j; l < j + 2; l++) {
-                    count += grid[k][l];
-                }
-            }
-
-            for (int k = i; k < i + 2; k++) {
-                for (int l = j; l < j + 2; l++) {
-                    tmp = count;
-                    tmp -= grid[k][l];
-                    answer = max(answer, tmp);
-                }
-            }
-        }
-    }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++) 
+            answer = max(answer, GetTropino(i, j));
 
     cout << answer;
 
