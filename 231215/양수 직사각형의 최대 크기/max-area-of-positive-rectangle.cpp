412111524e2ff86a8dd5795d25e5b75517875a8c
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-15 18:57:26 +0900

    Title: 양수 직사각형의 최대 크기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231215/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 231215/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -12,7 +12,7 @@ int GetSum(int x1, int y1, int x2, int y2) {
     int sum = 0;
     for (int i = x1; i <= x2; i++) {
         for (int j = y1; j <= y2; j++) {
-            if (grid[i][j] < 0) return -1;
+            if (grid[i][j] <= 0) return -1;
         }
     }
 
