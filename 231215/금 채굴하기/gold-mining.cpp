e89db5b3b7ac2e3153c3b1aa2ab4586a72275c7e
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-15 15:35:08 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231215/금 채굴하기/gold-mining.cpp
+++ 231215/금 채굴하기/gold-mining.cpp
@@ -19,7 +19,7 @@ int GetGold(int x, int y, int klimit) {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (abs(i - x) + abs(j - y) == klimit) {
+            if (abs(i - x) + abs(j - y) <= klimit) {
                 count += grid[i][j];
             }
         }
