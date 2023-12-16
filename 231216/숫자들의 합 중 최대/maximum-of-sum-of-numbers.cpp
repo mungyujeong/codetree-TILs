3af2c7cbf96dc5956df9b044f0f5ab6ce5e5d801
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 17:14:39 +0900

    Title: 숫자들의 합 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 231216/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 int x, y, answer = 0;
 
+int DigitSum(int x) {
+    if (x < 10)
+        return x;
+    
+    return DigitSum(x / 10) + x % 10;
+}
+
 int main() {
     cin >> x >> y;
-    for (int i = x; i <= y; i++) {
-        int d1 = i / 10000;
-        int d2 = (i / 1000) % 10;
-        int d3 = (i / 100) % 10;
-        int d4 = (i / 10) % 10;
-        int d5 = i % 10;
-
-        answer = max(answer, d1 + d2 + d3 + d4 + d5);
-    }
+    for (int i = x; i <= y; i++) 
+        answer = max(answer, DigitSum(i));
+        
+    
     cout << answer;
     return 0;
 }
