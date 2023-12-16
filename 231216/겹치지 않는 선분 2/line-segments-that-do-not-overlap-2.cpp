53a7ba7b2b0133731b001fdc50e291cf9533d39a
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 12:12:43 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231216/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 231216/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -26,29 +26,35 @@ int main() {
             if (sx2 - sx1 == fx2 - fx1) continue;
             // 둘다 부호가 양수일 경우
             if ((sx2 - sx1 > 0) && (fx2 - fx1 > 0)) {
-                if ((fx1 < sx1 && sx1 < fx2 && fx1 < sx2 && sx2 < fx2) ||
-                    (sx1 < fx1 && fx1 < sx2 && sx1 < fx2 && fx2 < sx2)) {
+                if (((fx1 < sx1 && sx1 < fx2) && (fx1 < sx2 && sx2 < fx2)) ||
+                    ((sx1 < fx1 && fx1 < sx2) && (sx1 < fx2 && fx2 < sx2))) {
                     crossed[i] = 1;
                     crossed[j] = 1;
                 }
             } // 부호가 둘다 음수일 경우 
             else if ((sx2 - sx1 < 0) && (fx2 - fx1 < 0)){
-                if ((fx2 < sx2 && sx2 < fx1 && fx2 < sx1 && sx1 < fx1) ||
-                    (sx2 < fx2 && fx2 < sx1 && sx2 < fx1 && fx1 < sx1)) {
+                if (((fx2 < sx2 && sx2 < fx1) && (fx2 < sx1 && sx1 < fx1)) ||
+                    ((sx2 < fx2 && fx2 < sx1) && (sx2 < fx1 && fx1 < sx1))) {
                     crossed[i] = 1;
                     crossed[j] = 1;
                 }
             } // 부호가 다를 경우 
-            else {
-                if ((fx1 < sx2 && sx2 < fx2 && fx2 < sx1) ||
-                    (fx1 < sx2 && sx2 < sx1 && sx1 < fx2) ||
-                    (sx2 < fx1 && fx1 < fx2 && fx2 < sx1) ||
-                    (sx1 < fx2 && fx2 < sx2 && sx2 < fx1) ||
-                    (sx1 < fx2 && fx2 < fx1 && fx1 < sx2) ||
-                    (fx2 < sx1 && sx1 < sx2 && sx2 < fx1)) {
+            else if ((sx2 - sx1 < 0) && (fx2 - fx1 > 0)){
+                if (((fx1 < sx2 && sx2 < fx2) && (sx2 < fx2 && fx2 < sx1)) ||
+                    ((fx1 < sx2 && sx2 < fx2) && (fx1 < sx1 && sx1 < fx2)) ||
+                    ((sx2 < fx1 && fx1 < sx1) && (sx2 < fx2 && fx2 < sx1)) ||
+                    ((sx2 < fx1 && fx1 < sx1) && (fx1 < sx1 && sx1 < fx2))) {
                         crossed[i] = 1;
                         crossed[j] = 1;
                     }
+            } else if ((sx2 - sx1 > 0) && (fx2 - fx1 < 0)){
+                if (((sx1 < fx2 && fx2 < sx2) && (fx2 < sx2 && sx2 < fx1)) ||
+                    ((sx1 < fx2 && fx2 < sx2) && (sx1 < fx1 && fx1 < sx2)) ||
+                    ((fx2 < sx1 && sx1 < fx2) && (fx2 < sx2 && sx2 < fx1)) ||
+                    ((fx2 < sx1 && sx1 < fx1) && (sx1 < fx1 && fx1 < sx2))) {
+                        crossed[i] = 1;
+                        crossed[j] = 1;
+                }
             }
         }
     }
