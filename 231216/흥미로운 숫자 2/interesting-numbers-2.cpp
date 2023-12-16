0ad9a4f5d302d967fe3ef4bbc2145abbdba0b4e9
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 18:29:44 +0900

    Title: 흥미로운 숫자 2, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 231216/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int x, y;
-int count = 0;
-int answer = 0;
+int x, y, answer = 0;
 
 int GetInterestingNum(int num) {
-    int count[10] = {0, };
-    int cnt = 0;
-    string str = to_string(num);
+    int count[10] = {0};
+    int allCnt = 0;
 
-    for (int i = 0; i < str.size(); i++) 
-        count[int(str[i] - '0')]++;
-
-    for (int i = 0; i < 10; i++) {
-        if (count[i] == str.size()) return 0;
-        if (count[i] > 0) {
-            cnt++;
-        }
+    while(num) {
+        count[num % 10]++;
+        allCnt++;
+        num /= 10;
     }
 
-    if (cnt > 2) return 0;
-    for (int i = 0; i < 10; i++) {
-        if (count[i] == str.size() - 1) 
+    for (int i = 0; i < 10; i++) 
+        if (count[i] == allCnt - 1)
             return 1;
-    }
     return 0;
 }
 
 int main() {
     cin >> x >> y;
-    for (int i = x; i <= y; i++) {
-        answer += GetInterestingNum(i);
-    }
 
+    for (int i = x; i <= y; i++) 
+        answer += GetInterestingNum(i);
+        
     cout << answer;
     return 0;
 }
