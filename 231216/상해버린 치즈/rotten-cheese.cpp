c2e3823420e9832e02ded33980742d6d019744cf
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 22:40:03 +0900

    Title: 상해버린 치즈, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/상해버린 치즈/rotten-cheese.cpp
+++ 231216/상해버린 치즈/rotten-cheese.cpp
@@ -1,64 +1,88 @@
 #include <iostream>
 
 #define MAX_N 50
+#define MAX_D 1000
 
 using namespace std;
 
-int n, m, d, s;
-int dArr[MAX_N][3];
-int sArr[MAX_N][2];
-int cheeze[MAX_N] = {0, };
-int candidate[MAX_N] = {0, };
+int n, m, d, s, answer;
 
-
-// 아픈 사람들 중 dm을 먹은 사람들을 찾고
-// 먹었던 시간이 st보다 작으면 true
-int findWhoEat() {
-    for (int i = 0; i < s; i++) {
-        // 환자 선택
-        int sp = sArr[i][0]; 
-        int st = sArr[i][1];
-
-        // sp가 먹었던 치즈 찾기
-        for (int j = 0; j < d; j++) {
-            int dp = dArr[j][0];
-            int dm = dArr[j][1];
-            int dt = dArr[j][2];
-
-            if (sp != dp) continue;
-            
-            // 먹었던 시간이 아팠던 시간 전이라면
-            if (dt < st) {
-                if (candidate[dm] == 1) {
-                    return dm;
-                }
-                candidate[dm] = 1;
-            }
-        }
+class Info1 {
+public:
+    int p, m, t;
+    Info1(int p, int m, int t) {
+        this->p = p;
+        this->m = m;
+        this->t = t;
     }
-}
+    Info1() {}
+};
+
+class Info2 {
+public:
+    int p, t;
+    Info2(int p, int t) {
+        this->p = p;
+        this->t = t;
+    }
+    Info2() {}
+};
+
+Info1 info1[MAX_D];
+Info2 info2[MAX_N];
 
 int main() {
     cin >> n >> m >> d >> s;
-    for (int i = 0; i < d; i++) 
-        cin >> dArr[i][0] >> dArr[i][1] >> dArr[i][2];
-    for (int i = 0; i < s; i++)
-        cin >> sArr[i][0] >> sArr[i][1];
 
-    int apple = findWhoEat();
-    // cout << apple << endl;
-
-    int answer = 0;
-    int p[MAX_N] = {0, };
-    for (int i = 0; i < d; i++) {
-        int dp = dArr[i][0];
-        int dm = dArr[i][1];
-        if (dm == apple) {
-            p[dp] = 1;
-        }
+    for (int i = 0; i < d; i++)  {
+        int p, m, t;
+        cin >> p >> m >> t;
+        info1[i] = Info1(p, m, t);
     }
-    for (int i = 0; i < MAX_N; i++)
-        answer += p[i];
+    
+    for (int i = 0; i < s; i++) {
+        int p, t;
+        cin >> p >> t;
+        info2[i] = Info2(p, t);
+    }
+
+    for (int i = 1; i <= m; i++) {
+        
+        int time[MAX_N + 1] = {};
+
+        // 해당 치즈를 먹은 최소 시간 구하기
+        for (int j = 0; j < d; j++) {
+            if(info1[j].m != i) continue;
+
+            int person = info1[j].p;
+            if (time[person] == 0) {
+                time[person] = info1[j].t;
+            } else if (time[person] > info1[j].t) {
+                time[person] = info1[j].t;
+            }
+        }
+
+        bool possible = true;
+
+        // 먹은 적이 없으면 time == 0
+        // 먹은 적은 있지만 환자가 된 시간보다 늦을 경우
+        for (int k = 0; k < s; k++) {
+            int persion = info2[k].p;
+            int s = info2[k].t;
+            if (time[persion] >= s) possible = false;
+            if (time[persion] == 0) possible = false;
+        }
+
+        int count = 0;
+        if (possible) {
+            for (int i = 1; i <= n; i++) {
+                if (time[i] != 0)
+                    count++;
+            }
+        }
+        answer = max(answer, count);
+    }
+    
     cout << answer;
     return 0;
 }
