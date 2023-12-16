9205c2ada90077c27f4c2f68a8f00099e8548074
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 16:09:33 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/개발자의 순위/developer's-rank.cpp
+++ 231216/개발자의 순위/developer's-rank.cpp
@@ -1,50 +1,46 @@
 #include <iostream>
 
-#define MAX_K 10
 #define MAX_N 20
 
 using namespace std;
 
 int k, n, answer = 0;
-int grid[MAX_N + 1][MAX_N + 1];
 int arr[MAX_N][MAX_N];
 
 int main() {
     cin >> k >> n;
-    for (int i = 0; i < k; i++) 
+    for (int i = 0; i < k; i++)
         for (int j = 0; j < n; j++)
             cin >> arr[i][j];
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
 
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            for (int k = j + 1; k < n; k++) {
-                int a = arr[i][j];
-                int b = arr[i][k];
+            if (i == j) continue;
 
-                if (grid[a][b] == 2) continue;
+            bool isTrue = true;
 
-                // 반대편이 없다면
-                if (grid[b][a] != 1) {
-                    grid[a][b] = 1;
-                } else {
-                    grid[a][b] = 2;
-                    grid[b][a] = 2;
+            for (int t = 0; t < k; t++) {
+                int index_j = 0;
+                int index_i = 0;
+
+                for (int q = 0; q < n; q++) {
+                    if (i == arr[t][q]) {
+                        index_i = q;
+                    } else if (j == arr[t][q]) {
+                        index_j = q;
+                    }
                 }
+
+                if (index_i > index_j)
+                    isTrue = false;
             }
+
+            if (isTrue)
+                answer++;
         }
-        // for (int t = 1; t < n; t++) {
-        //     for (int u = 1; u <= n; u++) 
-        //         cout << grid[t][u] << " ";
-        //     cout << endl;
-        // }
     }
 
-    for (int i = 0; i < MAX_N + 1; i++) 
-        for (int j = 0; j < MAX_N + 1; j++) 
-            if (grid[i][j] == 1)
-                answer++;
-
     cout << answer;
-
     return 0;
 }
