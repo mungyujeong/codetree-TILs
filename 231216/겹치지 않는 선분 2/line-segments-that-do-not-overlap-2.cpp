06bad1d64252d1c1b462be0a2748c575e4a5eb80
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-16 10:59:46 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231216/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 231216/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -22,18 +22,43 @@ int main() {
             int sx1 = x1[j];
             int sx2 = x2[j];
 
-            if ((fx1 < sx1 && sx1 < fx2 && fx1 < sx2 && sx2 < fx2) ||
-                (sx1 < fx1 && fx1 < sx2 && sx1 < fx2 && fx2 < sx2)) {
-                crossed[i] = 1;
-                crossed[j] = 1;
+            // 기울기가 같으면 pass
+            if (sx2 - sx1 == fx2 - fx1) continue;
+            // 둘다 부호가 양수일 경우
+            if ((sx2 - sx1 > 0) && (fx2 - fx1 > 0)) {
+                if ((fx1 < sx1 && sx1 < fx2 && fx1 < sx2 && sx2 < fx2) ||
+                    (sx1 < fx1 && fx1 < sx2 && sx1 < fx2 && fx2 < sx2)) {
+                    crossed[i] = 1;
+                    crossed[j] = 1;
+                }
+            } // 부호가 둘다 음수일 경우 
+            else if ((sx2 - sx1 < 0) && (fx2 - fx1 < 0)){
+                if ((fx2 < sx2 && sx2 < fx1 && fx2 < sx1 && sx1 < fx1) ||
+                    (sx2 < fx2 && fx2 < sx1 && sx2 < fx1 && fx1 < sx1)) {
+                    crossed[i] = 1;
+                    crossed[j] = 1;
+                }
+            } // 부호가 다를 경우 
+            else {
+                if ((fx1 < sx2 && sx2 < fx2 && fx2 < sx1) ||
+                    (fx1 < sx2 && sx2 < sx1 && sx1 < fx2) ||
+                    (sx2 < fx1 && fx1 < fx2 && fx2 < sx1) ||
+                    (sx1 < fx2 && fx2 < sx2 && sx2 < fx1) ||
+                    (sx1 < fx2 && fx2 < fx1 && fx1 < sx2) ||
+                    (fx2 < sx1 && sx1 < sx2 && sx2 < fx1)) {
+                        crossed[i] = 1;
+                        crossed[j] = 1;
+                    }
             }
         }
     }
     for (int i = 0; i < n; i++) {
+        // cout << crossed[i] << " ";
         if (crossed[i]) {
             answer--;
         }
     }
+    // cout << endl;
     cout << answer;
     return 0;
 }
