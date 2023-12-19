915718419dbd4263b8fbfe66b5678c228e7a69bd
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-19 11:20:11 +0900

    Title: 외판원 순회, Time: 1083ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 231219/외판원 순회/traveling-salesman-problem.cpp
+++ 231219/외판원 순회/traveling-salesman-problem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <climits>
 
 #define MAX_N 10
@@ -9,10 +10,19 @@ int n;
 bool visited[MAX_N + 1];
 int grid[MAX_N][MAX_N];
 int answer = INT_MAX;
+vector<pair<int, int>> v;
 
 void Cycle(int depth, int row, int cost, int cnt) {
     if (cnt == n - 1) {
-        answer = min(answer, cost + grid[row][0]);
+        
+        if (grid[row][0] != 0)
+            answer = min(answer, cost + grid[row][0]);  
+
+
+        // for (auto i : v)
+        //     cout << i.first << " " << i.second << endl;
+        // cout << "====================" << endl;
+        // cout << row << " " << cost + grid[row][0] << endl;
         return;
     }
 
@@ -24,8 +34,10 @@ void Cycle(int depth, int row, int cost, int cnt) {
         if (col == row) continue;
         if (grid[row][col] == 0) continue;
         visited[col] = true;
+        v.push_back({row, col});
         Cycle(depth + 1, col, cost + grid[row][col], cnt + 1);
         visited[col] = false;
+        v.pop_back();
         Cycle(depth + 1, col, cost + grid[row][col], cnt);
     }
 
