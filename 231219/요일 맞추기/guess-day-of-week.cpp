b2b411f336a426cd26a22dbd0ae867c5860b70a1
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-19 14:10:48 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231219/요일 맞추기/guess-day-of-week.cpp
+++ 231219/요일 맞추기/guess-day-of-week.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <tuple>
 
 using namespace std;
 
 int m1, m2, d1, d2;
-vector<pair<int, int>> v;
 string day[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -20,17 +16,11 @@ int GetDays(int m, int d) {
 
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
-    v.push_back({m1, d1});
-    v.push_back({m2, d2});
-    // sort(v.begin(), v.end());
-    // tie(m1, d1) = v[0];
-    // tie(m2, d2) = v[1];
 
     int diff = (GetDays(m2, d2) - GetDays(m1, d1));
-    // if (diff < 0) cout << (1 + diff) % 7;
-    // else cout << (1 + diff) % 7;
+    // cout << diff << endl;
+    if (diff < 0) cout << day[((diff + 1) % 7 + 7) % 7];
+    else cout << day[(diff + 1) % 7];
 
-    // cout << (1 + diff) % 7;
-    cout << day[(1 + diff) % 7];
     return 0;
 }
