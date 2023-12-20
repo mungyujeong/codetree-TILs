2b375041663d9170ea21baf0b2d26963902144be
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 12:55:51 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231220/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define OFFSET 1000
+#define MAX_R 2000
+
+using namespace std;
+
+int grid[MAX_R + 1][MAX_R + 1];
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        
+        x1 += OFFSET;
+        y1 += OFFSET;
+        x2 += OFFSET;
+        y2 += OFFSET;
+
+        for (int j = x1; j < x2; j++) {
+            for (int k = y1; k < y2; k++) {
+                grid[j][k] = i;
+            }
+        }
+    }
+    int answer = 0;
+    for (int i = 0; i <= MAX_R; i++) {
+        for (int j = 0; j <= MAX_R; j++) {
+            if (grid[i][j] == 1 || grid[i][j] == 3)
+                answer++;
+        }
+    }
+    cout << answer;
+    return 0;
+}
