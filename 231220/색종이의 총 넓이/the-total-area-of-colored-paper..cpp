8f7e415dcafca6ac8d1d3549fa6a0c421f49dfbd
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 13:04:31 +0900

    Title: 색종이의 총 넓이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231220/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
+++ 231220/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -13,6 +13,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         int x, y;
         cin >> x >> y;
+        x += OFFSET;
+        y += OFFSET;
         for (int j = x; j < x + 8; j++) {
             for (int k = y; k < y + 8; k++) {
                 grid[j][k] = 1;
