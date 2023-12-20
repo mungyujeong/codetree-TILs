1fbd4c161dadca7114a4b5186f4fe9cbcf627393
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 10:10:30 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231220/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#define MAX_N 100
+#define OFFSET 1000
+
+using namespace std;
+
+int n;
+int arr[MAX_N + OFFSET + 1];
+int x[MAX_N + 1];
+char c[MAX_N + 1];
+int curr_x = OFFSET;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> c[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (c[i] == 'L') {
+            for (int j = curr_x - x[i] + 1; j <= curr_x; j++) {
+                arr[j]++;
+            }
+            curr_x = curr_x - x[i];
+        } else {
+            for (int j = curr_x; j < curr_x + x[i]; j++) {
+                arr[j]++;
+            }
+            curr_x = curr_x + x[i];
+        }
+    }
+
+    int answer = 0;
+    for (int i = 0; i < MAX_N + OFFSET + 1; i++) {
+        if (arr[i] > 1) answer++;
+    }
+    cout << answer;
+    return 0;
+}
