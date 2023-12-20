1020f3235dca3c3f8f2bbb0daa00414101c3da78
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 09:57:03 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231220/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+int answer;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x1, x2;
+        cin >> x1 >> x2;
+        for (int j = x1; j <= x2; j++) {
+            arr[j]++;
+        }
+    }
+
+    for (int i = 0; i < MAX_N + 1; i++) {
+        answer = max(answer, arr[i]);
+    }
+
+    cout << answer;
+    return 0;
+}
