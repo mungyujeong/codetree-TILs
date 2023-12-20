5d376eaf34ca6e7292acfcad1f7c5288aec4d055
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 13:02:26 +0900

    Title: 색종이의 총 넓이, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231220/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#define OFFSET 100
+#define MAX_R 200
+
+using namespace std;
+
+int n;
+int grid[MAX_R + 1][MAX_R + 1];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        for (int j = x; j < x + 8; j++) {
+            for (int k = y; k < y + 8; k++) {
+                grid[j][k] = 1;
+            }
+        }
+    }
+
+    int answer = 0;
+
+    for (int i = 0; i <= MAX_R; i++) {
+        for (int j = 0; j <= MAX_R; j++) {
+            if (grid[i][j]) answer++;
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
