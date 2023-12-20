875a9ea1c49f184b039b152b8ef8ef1d8c32a595
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 12:48:24 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231220/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 231220/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -14,16 +14,16 @@ int main() {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
-        for (int j = x1; j < x2; j++) {
-            for (int k = y1; k < y2; k++) {
+        for (int j = x1 + OFFSET; j < x2 + OFFSET; j++) {
+            for (int k = y1 + OFFSET; k < y2 + OFFSET; k++) {
                 grid[j][k] = 1;
             }
         }
     }
 
     int answer = 0;
-    for (int i = 0; i < 2*OFFSET + 1; i++) {
-        for (int j  =0; j < 2*OFFSET + 1; j++) {
+    for (int i = 0; i < 2 * OFFSET + 1; i++) {
+        for (int j  =0; j < 2 * OFFSET + 1; j++) {
             if(grid[i][j] == 1) answer++;
         }
     }
