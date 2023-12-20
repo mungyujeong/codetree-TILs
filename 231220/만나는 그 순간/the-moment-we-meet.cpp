48e9415a1b9d39a4b2472d61af3ccbdf7b025235
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-20 13:55:33 +0900

    Title: 만나는 그 순간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231220/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+#define MAX_T 1000
+
+using namespace std;
+
+int n, m;
+int t;
+char d;
+int a[MAX_T + 1];
+int b[MAX_T + 1];
+int a_curr_x = 0;
+int b_curr_x = 0;
+int tmp_time = 0;
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> d >> t;
+
+        if (d == 'R') {
+            for (int j = tmp_time; j < tmp_time + t; j++) {
+                a[j + 1] = ++a_curr_x;
+            }
+        } else {
+            for (int j = tmp_time; j < tmp_time + t; j++) {
+                a[j + 1] = --a_curr_x;
+            }
+        }
+        tmp_time += t;
+    }
+    // for (int i = 0; i < MAX_T; i++) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
+    tmp_time = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> d >> t;  
+        if (d == 'R') {
+            for (int j = tmp_time; j < tmp_time + t; j++) {
+                b[j + 1] = ++b_curr_x;
+            }
+        } else {
+            for (int j = tmp_time; j < tmp_time + t; j++) {
+                b[j + 1] = --b_curr_x;
+            }
+        }
+        tmp_time += t;
+    }
+
+    int answer = -1;
+    for (int i = 0; i < MAX_T; i++) {
+        // cout << b[i] << " ";
+        if (a[i] == b[i] && a[i] != 0) {
+            answer = i + 1;
+            break;
+        }
+    }
+    if (answer == -1) cout << answer;
+    else cout << answer - 1;
+    return 0;
+}
