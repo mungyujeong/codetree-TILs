16c4285191481bfbfdf142083fd20a66fbca73bd
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-21 19:29:54 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 37ms, Memory: 0MB, Status: Passed - CodeTree

--- 231221/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
+++ 231221/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 int n;
-vector<char> v;
-vector<string> input;
-unordered_map<string, int> m;
+unordered_map<string, int> word_map;
 
 int main() {
     cin >> n;
+
     for (int i = 0; i < n; i++) {
-        string str;
-        cin >> str;
-
-        input.push_back(str);
-        v.clear();
-        for (int j = 0; j < str.size(); j++) {
-            v.push_back(str[j]);
-            // cout << str[j] << " ";
-        }
-        // cout << endl;
-
-        sort(v.begin(), v.end());
-
-        string tmp;
-        for (int j = 0; j < v.size(); j++) {
-            tmp += v[j];
-        }
-        // cout << tmp << endl;
-        input.push_back(tmp);
-        m[tmp] += 1;
+        string input_str;
+        cin >> input_str;
+        sort(input_str.begin(), input_str.end());
+        word_map[input_str]++;
     }
 
-    int answer = 0;
-    for (auto i : input) {
-        answer = max(answer, m[i]);
-    }
-    cout << answer;
+    int max_val = 0;
+    // for (unordered_map<string, int>::iterator iter = word_map.begin(); iter != word_map.end(); iter++)
+    for (auto i : word_map)
+        max_val = max(max_val, i.second);
+
+    cout << max_val;
     return 0;
 }
