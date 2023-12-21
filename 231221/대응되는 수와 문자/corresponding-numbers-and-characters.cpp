a9e8103df8936e93acf7db5181f21926e7089fb3
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-21 17:56:50 +0900

    Title: 대응되는 수와 문자, Time: 114ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 231221/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int n, m;
+unordered_map<string, int> str;
+unordered_map<int, string> num;
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string input;
+        cin >> input;
+        str[input] = i + 1;
+        num[i + 1] = input;
+    }
+
+    for (int i = 0; i < m; i++) {
+        string a;
+        cin >> a;
+        if ("0" < a && a < "9") {
+            cout << num[stoi(a)] << endl;
+        } else {
+            cout << str[a] << endl;
+        }
+    }
+    return 0;
+}
