37d54dfe2ad5f9987ec0eb99cb384c5bcd8e57a3
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-21 18:06:54 +0900

    Title: 대응되는 수와 문자, Time: 289ms, Memory: 16MB, Status: Passed - CodeTree

--- 231221/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
+++ 231221/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -20,7 +20,8 @@ int main() {
     for (int i = 0; i < m; i++) {
         string a;
         cin >> a;
-        if ("0" <= a && a <= "9") {
+        // cout << (int)a[0] - '0' << endl;
+        if (0 <= (int)a[0] - '0' && (int)a[0] - '0' <= 9) {
             cout << num[stoi(a)] << endl;
         } else {
             cout << str[a] << endl;
