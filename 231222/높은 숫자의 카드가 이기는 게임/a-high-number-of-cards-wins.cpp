82111ee92900b4e5b4adbb3a68c62df662120634
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-22 16:48:08 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231222/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
+++ 231222/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
@@ -1,33 +1,64 @@
 #include <iostream>
 #include <climits>
+#include <vector>
+#include <algorithm>
 
 #define MAX_N 50000
 
 using namespace std;
 
 int n;
-bool arr[MAX_N + 1] = {false, };
 int answer = 0;
+vector<int> a;
+vector<int> b;
+int arr[50001] = {};
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        arr[x] = true;
+        b.push_back(x);
+        arr[x] = 1;
+    }
+    sort(b.begin(), b.end());
+    for (int i = 1; i <= 2 * n; i++) {
+        if (arr[i] == 0) a.push_back(i);
     }
 
-    int curr = 0;
-    for (int i = 1; i <= 2 * n; i++) {
-        // cout << arr[i] << endl;
-        if (arr[i]) curr = i;
-        else {
-            if (curr < i) {
-                answer++;
-                curr = INT_MAX;
+    sort(a.begin(), a.end());
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << b[i] << " ";
+    // }
+    // cout << endl;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < b[i]) {
+            int origin = a[i];
+            for (int j = i; j < n; j++) {
+                if (j == i) continue;
+                if (a[j] > b[i]) {
+                    swap(a[i], a[j]);
+                    break;
+                }
             }
+            if (origin == a[i]) break;
         }
+        answer++;
     }
+
+    // int idx = 0;
+    // while (idx != n) {
+    //     if (a[idx] < b[idx]) {
+    //         swap(a[idx], a[idx + 1]);
+    //         continue;
+    //     } else {
+    //         answer++;
+    //         idx++;
+    //     }
+    // }
+    // cout << endl;
+
     cout << answer;
     return 0;
 }
