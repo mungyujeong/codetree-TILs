319290879469e20c06900ffbfdf052ea15d5c041
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-22 10:21:17 +0900

    Title: 두 수의 합, Time: 79ms, Memory: 6MB, Status: Passed - CodeTree

--- 231222/두 수의 합/sum-of-two-num.cpp
+++ 231222/두 수의 합/sum-of-two-num.cpp
@@ -15,26 +15,13 @@ int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        m[arr[i]] = k - arr[i];
-        cnt[arr[i]]++;
     }
 
     int answer = 0;
-    for (int i = 0; i < MAX_N; i++) {
-        // cout << arr[i] << " " << m[arr[i]] << endl;
-        if (cnt.find(arr[i]) != cnt.end() && cnt[m[arr[i]]] > 0) {
-            if (m[arr[i]] == arr[i]) {
-                cnt[m[arr[i]]]--;
-                answer += cnt[m[arr[i]]];
-                // cout << "cnt: " << cnt[m[arr[i]]] << " " << cnt[arr[i]] << endl;
-                // cnt[arr[i]]--;
-            } else {
-                answer += cnt[m[arr[i]]];
-                // cout << "cnt: " << cnt[m[arr[i]]] << " " << cnt[arr[i]] << endl;
-                cnt[m[arr[i]]]--;
-                cnt[arr[i]]--;
-            }
-        }
+
+    for (int i = 0; i < n; i++) {
+        answer += cnt[k - arr[i]];
+        cnt[arr[i]]++;
     }
 
     cout << answer;
