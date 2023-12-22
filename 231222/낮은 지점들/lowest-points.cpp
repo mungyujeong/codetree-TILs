db891571eea0a88b2f3b6b3201c76f6fcc3484e4
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-22 11:13:27 +0900

    Title: 낮은 지점들, Time: 71ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231222/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+unordered_map<long long, long long> m;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        long long x, y;
+        cin >> x >> y;
+        if (m.find(x) != m.end()) {
+            if (m[x] > y)
+                m[x] = y;
+        } else {
+            m[x] = y;
+        }
+    }
+    int answer = 0;
+    for (auto i : m) {
+        answer += i.second;
+    }
+
+    cout << answer;
+    return 0;
+}
