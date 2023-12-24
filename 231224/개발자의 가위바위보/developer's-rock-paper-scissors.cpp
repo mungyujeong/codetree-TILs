5a7c89156979e39ded0a071d9f9aff2609530aa7
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-24 12:50:04 +0900

    Title: 개발자의 가위바위보, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231224/개발자의 가위바위보/developer's-rock-paper-scissors.cpp
+++ 231224/개발자의 가위바위보/developer's-rock-paper-scissors.cpp
@@ -11,8 +11,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        if (a > b) cnt_a++;
-        else if (a < b) cnt_b++;
+        if (a == b) continue;
+        else if ((a + 1) % 3 + 1 == b) cnt_a++;
+        else cnt_b++;
     }
     cout << max(cnt_a, cnt_b);
     return 0;
