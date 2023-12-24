b0c6692d5bed2ee4f08ab02502c7d90515bad3b9
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-24 12:57:33 +0900

    Title: 연속된 숫자 만들기 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231224/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[3];
+
+int main() {
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr + 3);
+    int a = arr[0];
+    int b = arr[1];
+    int c = arr[2];
+
+    if (a + 1 == b && b + 1 == c) cout << 0;
+    else if (a + 2 == b || b + 2 == c) cout << 1;
+    else {
+        int diff = max(b - a - 1, c - b - 1);
+        cout << diff;
+    }
+    return 0;
+}
