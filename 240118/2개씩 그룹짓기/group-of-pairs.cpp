c3d6667dd0aa08a67931daa276148d32e653770f
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-18 16:57:58 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240118/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240118/2개씩 그룹짓기/group-of-pairs.cpp
@@ -8,11 +8,11 @@ int n;
 
 int main() {
     cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
+    vector<int> arr(2 * n);
+    for (int i = 0; i < 2 * n; i++)
         cin >> arr[i];
     
     sort(arr.begin(), arr.end());
-    cout << arr[n - 1] + arr[n - 2];
+    cout << arr[2 * n - 1] + arr[2 * n - 2];
     return 0;
 }
