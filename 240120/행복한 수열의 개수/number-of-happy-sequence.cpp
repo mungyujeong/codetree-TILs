a6c68bea255f35c41fab8c6ddf00adb52dcc8a62
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-20 09:24:45 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240120/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n, m;
+int board[MAX_N][MAX_N];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    int answer = 0;
+
+    for (int i = 0; i < n; i++) {
+        int before = board[i][0];
+        int cnt = 1;
+        for (int j = 1; j < n; j++) {
+            if (before == board[i][j]) cnt++;
+            else {
+                cnt = 1;
+                before = board[i][j];
+            }
+            if (cnt >= m) {
+                answer++;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int before = board[0][i];
+        int cnt = 1;
+        for (int j = 1; j < n; j++) {
+            if (before == board[j][i]) cnt++;
+            else {
+                cnt = 1;
+                before = board[j][i];
+            }
+            if (cnt >= m) {
+                answer++;
+                break;
+            }
+        }
+    }
+
+    cout << answer;
+    
+    return 0;
+}
