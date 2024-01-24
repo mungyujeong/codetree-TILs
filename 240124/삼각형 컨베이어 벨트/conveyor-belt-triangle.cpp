627a5f51e358532d0b16795cb6e7219494bfed5c
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-24 11:54:07 +0900

    Title: 삼각형 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240124/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
+++ 240124/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -12,8 +12,8 @@ int main() {
     cin.tie(0);
     
     cin >> n >> t;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < 3; j++)
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < n; j++)
             cin >> arr[i][j];
     
     while (t--) {
@@ -29,8 +29,8 @@ int main() {
             arr[i][0] = tmp[(i + 2) % 3];
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 3; j++)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < n; j++)
             cout << arr[i][j] << ' ';
         cout << endl;
     }
