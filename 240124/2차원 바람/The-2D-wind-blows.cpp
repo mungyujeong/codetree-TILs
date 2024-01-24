95ba5bb2f6525e12b486cc4314153ff1b4d8bf0c
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-24 14:15:39 +0900

    Title: 2차원 바람, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/2차원 바람/The-2D-wind-blows.cpp
+++ 240124/2차원 바람/The-2D-wind-blows.cpp
@@ -12,23 +12,20 @@ int dx[] = {0, 1, -1, 0, 0};
 int dy[] = {0, 0, 0, 1, -1};
 
 void wind() {
-    int tmp = board[r1][c2];
+    int tmp = board[r1][c1];
+    for (int i = r1; i < r2; i++)
+        board[i][c1] = board[i + 1][c1];
+
+    for (int i = c1; i < c2; i++) 
+        board[r2][i] = board[r2][i + 1];
+
+    for (int i = r2; i > r1; i--)
+        board[i][c2] = board[i - 1][c2];
+
     for (int i = c2; i > c1; i--)
         board[r1][i] = board[r1][i - 1];
     
-    int tmp2 = board[r2][c2];
-    for (int i = r2; i > r1 + 1; i--)
-        board[i][c2] = board[i - 1][c2];
-    board[r1 + 1][c2] = tmp;
-
-    int tmp3 = board[r2][c1];
-    for (int i = c1; i < c2 - 1; i++) 
-        board[r2][i] = board[r2][i + 1];
-    board[r2][c2 - 1] = tmp2;
-
-    for (int i = r1; i < r2 - 1; i++)
-        board[i][c1] = board[i + 1][c1];
-    board[r2 - 1][c1] = tmp3;
+    board[r1][c1 + 1] = tmp;
 }
 
 void average() {
