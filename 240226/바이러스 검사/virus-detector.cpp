64cc15659c1f0a95e01b9ebcb6336d80b6aa7da0
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-26 15:59:26 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define MAX_N 1'000'000
+
+using namespace std;
+using ll = long long;
+
+ll n, arr[MAX_N], a, b, answer;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> a >> b;
+    for (int i = 0; i < n; i++) {
+        answer++;
+        arr[i] -= a;
+        answer += arr[i] / b;
+        if (arr[i] % b > 0) answer++;
+    }
+
+    cout << answer;
+    return 0;
+}
