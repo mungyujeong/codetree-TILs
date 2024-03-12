106b589547c729b2cf8eeb46e637fd3b7491ccee
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-03-12 22:55:10 +0900

    Title: 이상한 다트 게임, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/이상한 다트 게임/odd-dart-game.cpp
+++ 240312/이상한 다트 게임/odd-dart-game.cpp
@@ -1,122 +1,114 @@
 #include <iostream>
-#include <queue>
-#include <tuple>
 
-#define MAX_N 51
+#define MAX_N 50
 
 using namespace std;
 
-int n, m, q, board[MAX_N][MAX_N], answer;
+int n, m, T, answer, board[MAX_N][MAX_N];
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
-bool changed;
+
+void Shift(int row, int d, int k) {
+    int tmp[MAX_N] = {};
+    
+    // 시계 방향
+    if (d == 0) {
+        for (int i = 0; i < m; i++)
+            tmp[(i + k) % m] = board[row][i];
+    }
+    else {
+        for (int i = 0; i < m; i++)
+            tmp[(i - k + m) % m] = board[row][i];
+    }
+
+    for (int i = 0; i < m; i++)
+        board[row][i] = tmp[i];
+}
 
 bool in_range(int x, int y) {
-    return 1 <= x && x <= n && 1 <= y && y <= m;
+    return 0 <= x && x < n && 0 <= y && y < m;
 }
 
-void rotate(int row, int d, int k) {
-    while (k--) {
-        // 시계방향
-        if (d == 0) {
-            int tmp = board[row][m];
-            for (int i = m; i > 1; i--)
-                board[row][i] = board[row][i - 1];
-            board[row][1] = tmp;
-        }
-        else {
-            int tmp = board[row][1];
-            for (int i = 1; i < m; i++)
-                board[row][i] = board[row][i + 1];
-            board[row][m] = tmp;
+bool Remove() {
+    bool check[MAX_N][MAX_N];
+    bool removed = false;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            check[i][j] = false;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j]) {
+                for (int d = 0; d < 4; d++) {
+                    int nx = i + dx[d];
+                    int ny = (j + dy[d] + m) % m;
+                    if (!in_range(nx, ny)) continue;
+                    if (board[nx][ny] == board[i][j])
+                        check[i][j] = check[nx][ny] = true;
+                }
+            }
         }
     }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (check[i][j]) {
+                removed = true;
+                board[i][j] = 0;
+            }
+    
+    return removed;
 }
 
-void bfs(int cx, int cy) {
-    queue<pair<int, int>> Q;
-    int visited[MAX_N][MAX_N] = {};
-    int cur = board[cx][cy];
-    int cnt = 0;
+void Normalize() {
+    int avg, cnt;
+    avg = cnt = 0;
 
-    Q.push({cx, cy});
-    visited[cx][cy] = 1;
-
-    while (!Q.empty()) {
-        int x, y;
-        tie(x, y) = Q.front(); Q.pop();
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (!in_range(nx, ny)) continue;
-            if (visited[nx][ny]) continue;
-            if (board[nx][ny] == cur) {
-                Q.push({nx, ny});
-                board[nx][ny] = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (board[i][j]) {
+                avg += board[i][j];
                 cnt++;
             }
-            visited[nx][ny] = 1;
-        }
-    }
 
-    if (cnt != 0) {
-        board[cx][cy] = 0;
-        changed = true;
+    if (cnt) {
+        avg /= cnt;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (board[i][j]) {
+                    if (board[i][j] > avg) board[i][j]--;
+                    else if (board[i][j] < avg) board[i][j]++;
+                }
     }
+}
+
+void Simulate(int x, int d, int k) {
+    for (int i = 0; i < n; i++) 
+        if ((i + 1) % x == 0)
+            Shift(i, d, k);
+    
+    if (!Remove())
+        Normalize();
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n >> m >> q;
-    for (int i = 1; i <= n; i++) 
-        for (int j = 1; j <= m; j++)
+    cin >> n >> m >> T;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> board[i][j];
     
-    while (q--) {
+    while (T--) {
         int x, d, k;
         cin >> x >> d >> k;
-        for (int i = x; i <= n; i+=x) 
-            rotate(i, d, k);
-
-        changed = false;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (board[i][j] == 0) continue;
-                bfs(i, j);
-            }
-        }
-
-        int avg = 0;
-        if (!changed) {
-            int cnt = 0;
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= m; j++) {
-                    if (board[i][j]) {
-                        avg += board[i][j];
-                        cnt++;
-                    }
-                }
-            }
-            avg /= cnt;
-        }
-
-        if (avg) {
-            for (int i = 1; i <= n; i++) 
-                for (int j = 1; j <= m; j++)
-                    if (board[i][j]) {
-                        if (board[i][j] > avg) board[i][j] -= 1;
-                        else if (board[i][j] < avg) board[i][j] += 1;
-                    }
-        } 
+        Simulate(x, d, k);
     }
 
-    for (int i = 1; i <= n; i++) 
-        for (int j = 1; j <= m; j++) 
-            if (board[i][j])
-                answer += board[i][j];
-
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            answer += board[i][j];
+    
     cout << answer;
-    return 0;
 }
